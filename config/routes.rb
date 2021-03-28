ad7c1a90574e570af1500273677860e41f03a997
Laura Messa Travin <laura.travin@gmail.com>
2021-03-28 20:35:39 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,14 @@ Rails.application.routes.draw do
   # end
 
 #new code widh  module
-scope '/admin', module: 'admin' do
+# scope '/admin', module: 'admin' do
+#  resources :stats, only: [:index]
+# end
+
+#When we want to route with a module and use that module's name as the URL prefix,
+# we can use the namespace method instead of scope, module.
+
+namespace :admin do
  resources :stats, only: [:index]
 end
 
